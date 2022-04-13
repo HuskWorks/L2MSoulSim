c87896b176ef99a6b056da523eee7fb404b2d910
HuskWorks <huskworks53@gmail.com>
2022-04-13 15:24:00 +0900

    ver.1 完成

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ import { itemData } from './data/itemData';
 import { values } from './types/values';
 
 const status: values = [
-  {point: 0, item1: 0, item2: 0, item3: 0, item4: 0, item5: 0, item6: 0, item7: 0, item8: 0, item9: 0},
+  { Point: 0, CriticalHit: 0, AllDamage: 0, Accuracy: 0, DoubleChance: 0, ExtraPvEDamage: 0, IgnoreDamageResistance: 0, ExtraPvPDamage: 0, IgnoreExtraReductionToStunned: 0, MaxHP: 0, PvPWeaponDefense: 0, CriticalHitResistance: 0, PvPSkillDefense: 0, WeaponBlock: 0, DamageReduction: 0, Defense: 0, ExtraDamageOnCriticalHit: 0, SkillDamageBoost: 0, WeaponDamageBoost: 0, AllTypeBoost: 0, BlockPenetration: 0, PvPDamageResistance: 0, PotionRecoveryRate: 0, HealBoost: 0, DoubleResistance: 0, DamageResistance: 0, CCAccuracy: 0, PvEAccuracy: 0, ExtraAccuracyToStunned: 0, PvPAccuracy: 0, SkillAccuracy: 0, AbsoluteAccuracy: 0, StunAccuracy: 0, HoldAccuracy: 0, SkillResistance: 0, CCDurationReduction: 0, HoldResistance: 0, WeaponDefense: 0, SkillDefense: 0, StunResistance: 0, AggressionResistance: 0, SilenceResistance: 0, CCStackResistance: 0, BlessingsRechargeIncrease: 0, BasicAttackDamage: 0, BasicAttackCriticalHit: 0, BasicAttackDamageResistance: 0, BasicAttackDamageReduction: 0, BasicAttackCriticalHitResistance: 0, BlessingsConservation: 0, BasicAttackDamageBoost: 0, BonusEXP: 0 },
 ];
 
 const data = {
@@ -108,7 +108,7 @@ const options = {
   plugins: {
     title: {
       display: true,
-      text: ' 魂吸収効果シミュレーター'
+      text: 'リネージュ2M 魂吸収効果シミュレーター'
     },
     legend: {
       display: false
@@ -122,14 +122,9 @@ const options = {
       },
       onDrag: function (e: any, datasetIndex: number, index: number, value: number) {
         e.target.style.cursor = 'grabbing'
-        //console.log(e, datasetIndex, index, value)
       },
       onDragEnd: function (e: any, datasetIndex: number, index: number, value: number) {
         e.target.style.cursor = 'default'
-        //data.labels[index] = value.toString().padStart(2, '0')
-        //data.labels[index] = data.labels[index].replace(/\[.*?\]/, `[${value.toString().padStart(2, '0')}]`)
-        //myChart.update()
-        //console.log(myChart.data.datasets[0].data)
         update(index, value);
       }
     }
@@ -142,7 +137,7 @@ function update(index: number, value: number) {
   myChart.update();
 
   // 初期化
-  status[0] = {point: 0, item1: 0, item2: 0, item3: 0, item4: 0, item5: 0, item6: 0, item7: 0, item8: 0, item9: 0}
+  status[0] = { Point: 0, CriticalHit: 0, AllDamage: 0, Accuracy: 0, DoubleChance: 0, ExtraPvEDamage: 0, IgnoreDamageResistance: 0, ExtraPvPDamage: 0, IgnoreExtraReductionToStunned: 0, MaxHP: 0, PvPWeaponDefense: 0, CriticalHitResistance: 0, PvPSkillDefense: 0, WeaponBlock: 0, DamageReduction: 0, Defense: 0, ExtraDamageOnCriticalHit: 0, SkillDamageBoost: 0, WeaponDamageBoost: 0, AllTypeBoost: 0, BlockPenetration: 0, PvPDamageResistance: 0, PotionRecoveryRate: 0, HealBoost: 0, DoubleResistance: 0, DamageResistance: 0, CCAccuracy: 0, PvEAccuracy: 0, ExtraAccuracyToStunned: 0, PvPAccuracy: 0, SkillAccuracy: 0, AbsoluteAccuracy: 0, StunAccuracy: 0, HoldAccuracy: 0, SkillResistance: 0, CCDurationReduction: 0, HoldResistance: 0, WeaponDefense: 0, SkillDefense: 0, StunResistance: 0, AggressionResistance: 0, SilenceResistance: 0, CCStackResistance: 0, BlessingsRechargeIncrease: 0, BasicAttackDamage: 0, BasicAttackCriticalHit: 0, BasicAttackDamageResistance: 0, BasicAttackDamageReduction: 0, BasicAttackCriticalHitResistance: 0, BlessingsConservation: 0, BasicAttackDamageBoost: 0, BonusEXP: 0 };
   document.querySelector('#status-view > table > tbody')!.innerHTML = '';
 
   // ポイント更新
@@ -156,40 +151,124 @@ function update(index: number, value: number) {
 
     // 項目ごとの設定値から0~現在値までポイントを加算
     for (let j: number = 0; j <= data; j++) {
-      status[0].point += values[j].point;
-      status[0].item1 += values[j].item1;
-      status[0].item2 += values[j].item2;
-      status[0].item3 += values[j].item3;
-      status[0].item4 += values[j].item4;
-      status[0].item5 += values[j].item5;
-      status[0].item6 += values[j].item6;
-      status[0].item7 += values[j].item7;
-      status[0].item8 += values[j].item8;
-      status[0].item9 += values[j].item9;
+      status[0].Point += values[j].Point
+      status[0].CriticalHit += values[j].CriticalHit
+      status[0].AllDamage += values[j].AllDamage
+      status[0].Accuracy += values[j].Accuracy
+      status[0].DoubleChance += values[j].DoubleChance
+      status[0].ExtraPvEDamage += values[j].ExtraPvEDamage
+      status[0].IgnoreDamageResistance += values[j].IgnoreDamageResistance
+      status[0].ExtraPvPDamage += values[j].ExtraPvPDamage
+      status[0].IgnoreExtraReductionToStunned += values[j].IgnoreExtraReductionToStunned
+      status[0].MaxHP += values[j].MaxHP
+      status[0].PvPWeaponDefense += values[j].PvPWeaponDefense
+      status[0].CriticalHitResistance += values[j].CriticalHitResistance
+      status[0].PvPSkillDefense += values[j].PvPSkillDefense
+      status[0].WeaponBlock += values[j].WeaponBlock
+      status[0].DamageReduction += values[j].DamageReduction
+      status[0].Defense += values[j].Defense
+      status[0].ExtraDamageOnCriticalHit += values[j].ExtraDamageOnCriticalHit
+      status[0].SkillDamageBoost += values[j].SkillDamageBoost
+      status[0].WeaponDamageBoost += values[j].WeaponDamageBoost
+      status[0].AllTypeBoost += values[j].AllTypeBoost
+      status[0].BlockPenetration += values[j].BlockPenetration
+      status[0].PvPDamageResistance += values[j].PvPDamageResistance
+      status[0].PotionRecoveryRate += values[j].PotionRecoveryRate
+      status[0].HealBoost += values[j].HealBoost
+      status[0].DoubleResistance += values[j].DoubleResistance
+      status[0].DamageResistance += values[j].DamageResistance
+      status[0].CCAccuracy += values[j].CCAccuracy
+      status[0].PvEAccuracy += values[j].PvEAccuracy
+      status[0].ExtraAccuracyToStunned += values[j].ExtraAccuracyToStunned
+      status[0].PvPAccuracy += values[j].PvPAccuracy
+      status[0].SkillAccuracy += values[j].SkillAccuracy
+      status[0].AbsoluteAccuracy += values[j].AbsoluteAccuracy
+      status[0].StunAccuracy += values[j].StunAccuracy
+      status[0].HoldAccuracy += values[j].HoldAccuracy
+      status[0].SkillResistance += values[j].SkillResistance
+      status[0].CCDurationReduction += values[j].CCDurationReduction
+      status[0].HoldResistance += values[j].HoldResistance
+      status[0].WeaponDefense += values[j].WeaponDefense
+      status[0].SkillDefense += values[j].SkillDefense
+      status[0].StunResistance += values[j].StunResistance
+      status[0].AggressionResistance += values[j].AggressionResistance
+      status[0].SilenceResistance += values[j].SilenceResistance
+      status[0].CCStackResistance += values[j].CCStackResistance
+      status[0].BlessingsRechargeIncrease += values[j].BlessingsRechargeIncrease
+      status[0].BasicAttackDamage += values[j].BasicAttackDamage
+      status[0].BasicAttackCriticalHit += values[j].BasicAttackCriticalHit
+      status[0].BasicAttackDamageResistance += values[j].BasicAttackDamageResistance
+      status[0].BasicAttackDamageReduction += values[j].BasicAttackDamageReduction
+      status[0].BasicAttackCriticalHitResistance += values[j].BasicAttackCriticalHitResistance
+      status[0].BlessingsConservation += values[j].BlessingsConservation
+      status[0].BasicAttackDamageBoost += values[j].BasicAttackDamageBoost
+      status[0].BonusEXP += values[j].BonusEXP
     }
   }
-  
-  document.getElementById('point')!.textContent = status[0].point.toString();
+
+  document.getElementById('point')!.textContent = status[0].Point.toString();
 
   // ステータス更新
-  if (status[0].item1 > 0) addRow('item1', status[0].item1.toString());
-  if (status[0].item2 > 0) addRow('item2', status[0].item2.toString());
-  if (status[0].item3 > 0) addRow('item3', status[0].item3.toString());
-  if (status[0].item4 > 0) addRow('item4', status[0].item4.toString());
-  if (status[0].item5 > 0) addRow('item5', status[0].item5.toString());
-  if (status[0].item6 > 0) addRow('item6', status[0].item6.toString());
-  if (status[0].item7 > 0) addRow('item7', status[0].item7.toString());
-  if (status[0].item8 > 0) addRow('item8', status[0].item8.toString());
-  if (status[0].item9 > 0) addRow('item9', status[0].item9.toString());
+  if (status[0].CriticalHit > 0) addRow('クリティカル', status[0].CriticalHit.toString(), '%');
+  if (status[0].AllDamage > 0) addRow('すべてのダメージ', status[0].AllDamage.toString());
+  if (status[0].Accuracy > 0) addRow('命中', status[0].Accuracy.toString());
+  if (status[0].DoubleChance > 0) addRow('ダブル確率', status[0].DoubleChance.toString(), '%');
+  if (status[0].ExtraPvEDamage > 0) addRow('PvE追加ダメージ', status[0].ExtraPvEDamage.toString());
+  if (status[0].IgnoreDamageResistance > 0) addRow('ダメージ耐性無視', status[0].IgnoreDamageResistance.toString(), '%');
+  if (status[0].ExtraPvPDamage > 0) addRow('PvP追加ダメージ', status[0].ExtraPvPDamage.toString());
+  if (status[0].IgnoreExtraReductionToStunned > 0) addRow('スタン対象追加リダクション無視', status[0].IgnoreExtraReductionToStunned.toString());
+  if (status[0].MaxHP > 0) addRow('最大HP', status[0].MaxHP.toString());
+  if (status[0].PvPWeaponDefense > 0) addRow('PvP武器ダメージ耐性', status[0].PvPWeaponDefense.toString(), '%');
+  if (status[0].CriticalHitResistance > 0) addRow('クリティカル抵抗', status[0].CriticalHitResistance.toString(), '%');
+  if (status[0].PvPSkillDefense > 0) addRow('PvPスキルダメージ耐性', status[0].PvPSkillDefense.toString(), '%');
+  if (status[0].WeaponBlock > 0) addRow('武器防御', status[0].WeaponBlock.toString(), '%');
+  if (status[0].DamageReduction > 0) addRow('ダメージリダクション', status[0].DamageReduction.toString());
+  if (status[0].Defense > 0) addRow('防御力', status[0].Defense.toString());
+  if (status[0].ExtraDamageOnCriticalHit > 0) addRow('クリティカル時の追加ダメージ', status[0].ExtraDamageOnCriticalHit.toString());
+  if (status[0].SkillDamageBoost > 0) addRow('スキルダメージ増幅', status[0].SkillDamageBoost.toString(), '%');
+  if (status[0].WeaponDamageBoost > 0) addRow('武器ダメージ増幅', status[0].WeaponDamageBoost.toString(), '%');
+  if (status[0].AllTypeBoost > 0) addRow('すべての属性増幅', status[0].AllTypeBoost.toString(), '%');
+  if (status[0].BlockPenetration > 0) addRow('防御貫通', status[0].BlockPenetration.toString(), '%');
+  if (status[0].PvPDamageResistance > 0) addRow('PvPダメージ耐性', status[0].PvPDamageResistance.toString(), '%');
+  if (status[0].PotionRecoveryRate > 0) addRow('ポーション回復率', status[0].PotionRecoveryRate.toString(), '%');
+  if (status[0].HealBoost > 0) addRow('ヒール増幅', status[0].HealBoost.toString(), '%');
+  if (status[0].DoubleResistance > 0) addRow('ダブル抵抗', status[0].DoubleResistance.toString(), '%');
+  if (status[0].DamageResistance > 0) addRow('ダメージ耐性', status[0].DamageResistance.toString(), '%');
+  if (status[0].CCAccuracy > 0) addRow('状態異常命中', status[0].CCAccuracy.toString(), '%');
+  if (status[0].PvEAccuracy > 0) addRow('PvE命中', status[0].PvEAccuracy.toString());
+  if (status[0].ExtraAccuracyToStunned > 0) addRow('スタン対象追加命中', status[0].ExtraAccuracyToStunned.toString());
+  if (status[0].PvPAccuracy > 0) addRow('PvP命中', status[0].PvPAccuracy.toString());
+  if (status[0].SkillAccuracy > 0) addRow('スキル命中', status[0].SkillAccuracy.toString());
+  if (status[0].AbsoluteAccuracy > 0) addRow('絶対命中率', status[0].AbsoluteAccuracy.toString(), '%');
+  if (status[0].StunAccuracy > 0) addRow('スタン命中', status[0].StunAccuracy.toString(), '%');
+  if (status[0].HoldAccuracy > 0) addRow('ホールド命中', status[0].HoldAccuracy.toString(), '%');
+  if (status[0].SkillResistance > 0) addRow('スキル抵抗', status[0].SkillResistance.toString());
+  if (status[0].CCDurationReduction > 0) addRow('状態異常時間減少', status[0].CCDurationReduction.toString(), '%');
+  if (status[0].HoldResistance > 0) addRow('ホールド耐性', status[0].HoldResistance.toString(), '%');
+  if (status[0].WeaponDefense > 0) addRow('武器ダメージ耐性', status[0].WeaponDefense.toString(), '%');
+  if (status[0].SkillDefense > 0) addRow('スキルダメージ耐性', status[0].SkillDefense.toString(), '%');
+  if (status[0].StunResistance > 0) addRow('スタン耐性', status[0].StunResistance.toString(), '%');
+  if (status[0].AggressionResistance > 0) addRow('ヘイト耐性', status[0].AggressionResistance.toString(), '%');
+  if (status[0].SilenceResistance > 0) addRow('沈黙耐性', status[0].SilenceResistance.toString(), '%');
+  if (status[0].CCStackResistance > 0) addRow('重複状態異常耐性', status[0].CCStackResistance.toString(), '%');
+  if (status[0].BlessingsRechargeIncrease > 0) addRow('祝福増加量増加', status[0].BlessingsRechargeIncrease.toString(), '%');
+  if (status[0].BasicAttackDamage > 0) addRow('通常攻撃ダメージ', status[0].BasicAttackDamage.toString());
+  if (status[0].BasicAttackCriticalHit > 0) addRow('通常攻撃クリティカル', status[0].BasicAttackCriticalHit.toString(), '%');
+  if (status[0].BasicAttackDamageResistance > 0) addRow('通常攻撃ダメージ耐性', status[0].BasicAttackDamageResistance.toString(), '%');
+  if (status[0].BasicAttackDamageReduction > 0) addRow('通常攻撃ダメージリダクション', status[0].BasicAttackDamageReduction.toString());
+  if (status[0].BasicAttackCriticalHitResistance > 0) addRow('通常攻撃クリティカル抵抗', status[0].BasicAttackCriticalHitResistance.toString(), '%');
+  if (status[0].BlessingsConservation > 0) addRow('祝福消耗量減少', status[0].BlessingsConservation.toString(), '%');
+  if (status[0].BasicAttackDamageBoost > 0) addRow('通常攻撃ダメージ増幅', status[0].BasicAttackDamageBoost.toString(), '%');
+  if (status[0].BonusEXP > 0) addRow('EXPボーナス', status[0].BonusEXP.toString(), '%');
 }
 
-function addRow(itemName: string, value: string) {
+function addRow(itemName: string, value: string, unit: string = '') {
   const table: HTMLTableElement = <HTMLTableElement>document.querySelector('#status-view > table');
   const row: HTMLTableRowElement = table.insertRow(-1);
   const cell1: HTMLTableCellElement = row.insertCell(-1);
   const cell2: HTMLTableCellElement = row.insertCell(-1);
   cell1.innerHTML = itemName;
-  cell2.innerHTML = value;
+  cell2.innerHTML = `+${value}${unit}`;
 }
 
 const ctx = document.querySelector('#chart-view > canvas');
--- src/types/values.ts
+++ src/types/values.ts
@@ -1,12 +1,54 @@
 export type values = {
-  point: number,
-  item1: number,
-  item2: number,
-  item3: number,
-  item4: number,
-  item5: number,
-  item6: number,
-  item7: number,
-  item8: number,
-  item9: number,
+  Point: number,                          // 1.ポイント
+  CriticalHit: number,                    // 2.クリティカル
+  AllDamage: number,                      // 3.すべてのダメージ
+  Accuracy: number,                       // 4.命中
+  DoubleChance: number,                   // 5.ダブル確率
+  ExtraPvEDamage: number,                 // 6.PvE追加ダメージ
+  IgnoreDamageResistance: number,         // 7.ダメージ耐性無視
+  ExtraPvPDamage: number,                 // 8.PvP追加ダメージ
+  IgnoreExtraReductionToStunned: number,  // 9.スタン対象追加リダクション無視
+  MaxHP: number,                          // 10.最大HP
+  PvPWeaponDefense: number,               // 11.PvP武器ダメージ耐性
+  CriticalHitResistance: number,          // 12.クリティカル抵抗
+  PvPSkillDefense: number,                // 13.PvPスキルダメージ耐性
+  WeaponBlock: number,                    // 14.武器防御
+  DamageReduction: number,                // 15.ダメージリダクション
+  Defense: number,                        // 16.防御力
+  ExtraDamageOnCriticalHit: number,       // 17.クリティカル時の追加ダメージ
+  SkillDamageBoost: number,               // 18.スキルダメージ増幅
+  WeaponDamageBoost: number,              // 19.武器ダメージ増幅
+  AllTypeBoost: number,                   // 20.すべての属性増幅
+  BlockPenetration: number,               // 21.防御貫通
+  PvPDamageResistance: number,            // 22.PvPダメージ耐性
+  PotionRecoveryRate: number,             // 23.ポーション回復率
+  HealBoost: number,                      // 24.ヒール増幅
+  DoubleResistance: number,               // 25.ダブル抵抗
+  DamageResistance: number,               // 26.ダメージ耐性
+  CCAccuracy: number,                     // 27.状態異常命中
+  PvEAccuracy: number,                    // 28.PvE命中
+  ExtraAccuracyToStunned: number,         // 29.スタン対象追加命中
+  PvPAccuracy: number,                    // 30.PvP命中
+  SkillAccuracy: number,                  // 31.スキル命中
+  AbsoluteAccuracy: number,               // 32.絶対命中率
+  StunAccuracy: number,                   // 33.スタン命中
+  HoldAccuracy: number,                   // 34.ホールド命中
+  SkillResistance: number,                // 35.スキル抵抗
+  CCDurationReduction: number,            // 36.状態異常時間減少
+  HoldResistance: number,                 // 37.ホールド耐性
+  WeaponDefense: number,                  // 38.武器ダメージ耐性
+  SkillDefense: number,                   // 39.スキルダメージ耐性
+  StunResistance: number,                 // 40.スタン耐性
+  AggressionResistance: number,           // 41.ヘイト耐性
+  SilenceResistance: number,              // 42.沈黙耐性
+  CCStackResistance: number,              // 43.重複状態異常耐性
+  BlessingsRechargeIncrease: number,        // 44.祝福増加量増加
+  BasicAttackDamage: number,                // 45.通常攻撃ダメージ
+  BasicAttackCriticalHit: number,           // 46.通常攻撃クリティカル
+  BasicAttackDamageResistance: number,      // 47.通常攻撃ダメージ耐性
+  BasicAttackDamageReduction: number,       // 48.通常攻撃ダメージリダクション
+  BasicAttackCriticalHitResistance: number, // 49.通常攻撃クリティカル抵抗
+  BlessingsConservation: number,            // 50.祝福消耗量減少
+  BasicAttackDamageBoost: number,           // 51.通常攻撃ダメージ増幅
+  BonusEXP: number,                         // 52.EXPボーナス
 }[];
