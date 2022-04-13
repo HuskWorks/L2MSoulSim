a40bbdc504aed519074d1e548cf20a0da31524e4
HuskWorks <huskworks53@gmail.com>
2022-04-13 15:35:03 +0900

    ver.1 公開

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ const config = {
   //},
   entry: path.join(__dirname, 'src', 'index.ts'),
   output: {
-    path: path.join(__dirname, 'dist'),
+    //path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'docs'),
     filename: 'bundle.js'
   },
   module: {
@@ -54,7 +55,8 @@ const config = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      //directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, 'docs'),
     },
     port: 48080,
     open: true
